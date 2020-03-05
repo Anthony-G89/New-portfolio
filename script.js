2ba7c448d3bf03ca748a6f1dc5ffff84994e5f37
Anthony Guerrero <adguerrero1935@gmail.com>
2020-03-05 17:51:39 -0500

    added weather Dashboard to my portfolio

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const toggle = $("#toggle");
+ const weatherElm = document.querySelector(".weatherText");
 
 $("#toggle").on("click",() =>{
     const element = document.body;
     element.classList.toggle("dark-mode");
     
 });
+
+$(".weatherText").on("click", () =>{
+    window.open("https://knuckleh3ad89.github.io/weather-dashboard/");
+    
+})
