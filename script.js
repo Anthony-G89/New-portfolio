aa7554d23c5706b3250fc5758608c5ce72c05cfe
Anthony Guerrero <adguerrero1935@gmail.com>
2020-03-05 19:46:26 -0500

    Added some of my HW to my portfolio

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
 const toggle = $("#toggle");
- const weatherElm = document.querySelector(".weatherText");
+ const weatherElm = $(".weatherText");
+ const codeQuizElm = $(".codeQuizText");
+ const localWireElm = $("LocalWireText");
 
-$("#toggle").on("click",() =>{
-    const element = document.body;
-    element.classList.toggle("dark-mode");
+// $("#toggle").on("click",() =>{
+//     const element = document.body;
+//     element.classList.toggle("dark-mode");
     
-});
+// });
 
 $(".weatherText").on("click", () =>{
     window.open("https://knuckleh3ad89.github.io/weather-dashboard/");
     
+});
+
+$(".codeQuizText").on("click", ()=>{
+    window.open("https://knuckleh3ad89.github.io/Code-Quiz/");
+
+});
+
+$(".LocalWireText").on("click",()=>{
+    window.open("https://knuckleh3ad89.github.io/Project_One/");
 })
