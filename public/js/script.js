c47abe3de563d14250fad97f70efdd872fa04c0a
Anthony Guerrero <adguerrero1935@gmail.com>
2020-04-21 17:32:56 -0400

    added password HW

--- public/js/script.js
+++ public/js/script.js
@@ -1,29 +1,33 @@
 const toggle = $("#toggle");
- const weatherElm = $(".weatherText");
- const codeQuizElm = $(".codeQuizText");
- const localWireElm = $("LocalWireText");
- const goodReadMe = $(".readMeText");
+const weatherElm = $(".weatherText");
+const codeQuizElm = $(".codeQuizText");
+const localWireElm = $("LocalWireText");
+const goodReadMe = $(".readMeText");
 
 // $("#toggle").on("click",() =>{
 //     const element = document.body;
 //     element.classList.toggle("dark-mode");
-    
+
 // });
 
-$("#weatherDashboard").on("click", () =>{
+$("#weatherDashboard").on("click", () => {
     window.open("https://github.com/knuckleh3ad89/weather-dashboard");
-    
+
 });
 
-$("#codeQuiz").on("click", ()=>{
+$("#codeQuiz").on("click", () => {
     window.open("https://github.com/knuckleh3ad89/Code-Quiz");
 
 });
 
-$("#LocalWire").on("click",()=>{
+$("#LocalWire").on("click", () => {
     window.open("https://github.com/knuckleh3ad89/Project_One");
 });
 
-$("#readMe").on("click", ()=>{
+$("#readMe").on("click", () => {
     window.open("https://github.com/knuckleh3ad89/good_readme");
-})
+});
+
+$("#passwordGenerator").on("click", () => {
+    window.open("https://knuckleh3ad89.github.io/Password-Generator/");
+});
