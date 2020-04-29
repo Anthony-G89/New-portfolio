9e9137348191d88c6abb202dec13def266747241
Anthony Guerrero <adguerrero1935@gmail.com>
2020-04-29 19:01:49 -0400

    moved html out of public folder

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,8 @@ const weatherElm = $(".weatherText");
 const codeQuizElm = $(".codeQuizText");
 const localWireElm = $("LocalWireText");
 const goodReadMe = $(".readMeText");
+const stackChat = $("#stackChat");
+
 
 // $("#toggle").on("click",() =>{
 //     const element = document.body;
@@ -31,3 +33,21 @@ $("#readMe").on("click", () => {
 $("#passwordGenerator").on("click", () => {
     window.open("https://knuckleh3ad89.github.io/Password-Generator/");
 });
+
+$("#workDay").on("click", () => {
+    window.open("https://knuckleh3ad89.github.io/work-day-scheduler/");
+});
+
+
+$("#stackChat").on("click", () => {
+    window.open("https://stackchatapp.herokuapp.com/");
+});
+
+$("#teamTemplate").on("click", () =>{
+    window.open("https://github.com/knuckleh3ad89/Team_Template");
+});
+
+$("#noteTaker").on("click", () =>{
+    window.open("https://github.com/knuckleh3ad89/week-11-note-taker");
+});
+
